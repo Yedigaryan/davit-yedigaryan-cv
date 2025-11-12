8b297b7f1025a007f6780ae12c6af7650e021e23
Davit Yedigaryan <davit.yedigaryan.1993@gmail.com>
2025-11-13 01:15:15 +0400

    Fixed correct redirection with location reload

---
+++ _next/static/a-MsvKPq3SFLSKDHpcfR8/_buildManifest.js
@@ -0,0 +1,14 @@
+self.__BUILD_MANIFEST = {
+  "/_error": [
+    "./static/chunks/c205cec7d1a4b416.js"
+  ],
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
