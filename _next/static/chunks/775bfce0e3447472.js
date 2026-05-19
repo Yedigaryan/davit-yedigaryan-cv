ae5ee6139aadf69836afb60f41a8585413faaf22
Davit Yedigaryan <davit.yedigaryan.1993@gmail.com>
2026-05-19 04:33:16 +0400

    Deploy build from main@3d7b15b
    Source: feat: add automated FTP deployment script and update documentation
    Built:  2026-05-19 00:33:16 UTC

---
+++ _next/static/chunks/775bfce0e3447472.js
@@ -0,0 +1,5 @@
+__turbopack_load_page_chunks__("/_app", [
+  "static/chunks/b8f5a32d04fb0b00.js",
+  "static/chunks/3c754a0c1cca278b.js",
+  "static/chunks/turbopack-e79cbb4de56af85f.js"
+])
---
+++ _next/static/chunks/db92df1e3be17a09.js
@@ -0,0 +1,5 @@
+__turbopack_load_page_chunks__("/_error", [
+  "static/chunks/f38feaa434ba2f91.js",
+  "static/chunks/3c754a0c1cca278b.js",
+  "static/chunks/turbopack-0e1fdaf848b8ff1c.js"
+])
